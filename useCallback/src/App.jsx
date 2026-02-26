7561ee52b5bae5e22e7266ffd9570ff944ff1037
JuanAndresA016 <juancasthu@gmail.com>
2026-02-26 17:57:23 -0600

    Cambios

--- useCallback/src/App.jsx
+++ useCallback/src/App.jsx
@@ -7,25 +7,18 @@ const Boton = React.memo(({ alHacerClick, texto }) => {
 });
 
 export default function ContadorApp() {
-  const [contador1, setContador1] = useState(0);
-  const [contador2, setContador2] = useState(0);
-
-  // Sin useCallback, esta función se recrea en cada renderizado
-  const incrementar1 = () => setContador1(c => c + 1);
+  const [contador, setContador] = useState(0);
 
   // Con useCallback, la función se mantiene estable
-  const incrementar2 = useCallback(() => {
-    setContador2(c => c + 1);
+  const incrementar = useCallback(() => {
+    setContador(c => c + 1);
   }, []); // Dependencias vacías: no cambia nunca
 
   return (
     <>
-      <p>Contador 1: {contador1}</p>
-      <p>Contador 2: {contador2}</p>
-      {/* Botón 1 se renderiza de nuevo siempre */}
-      <Boton alHacerClick={incrementar1} texto="Incrementar 1" />
+      <p>Contador: {contador}</p>
       {/* Botón 2 se mantiene igual */}
-      <Boton alHacerClick={incrementar2} texto="Incrementar 2" />
+      <Boton alHacerClick={incrementar} texto="Incrementar" />
     </>
   );
 }
