ed116ddaae5cf171534287f27bfbb19e79973f45
emiduran05 <emiduranf05@gmail.com>
2026-02-27 17:29:29 -0600

    ejemplo

--- useCallback/src/App.jsx
+++ useCallback/src/App.jsx
@@ -7,15 +7,27 @@ const Boton = React.memo(({ alHacerClick, texto }) => {
 
 export default function ContadorApp() {
   const [contador, setContador] = useState(0);
+  const [bool, setBool] = useState(false);
 
   const incrementar = useCallback(() => {
     setContador(c => c + 1);
   }, []);
 
+  const incrementarMal = () => {
+    setContador(c => c +1);
+  }
+
+  const cambiarBool = useCallback(() => {
+    setBool(b => !b);
+  }, [bool]);
+
   return (
     <>
       <p>Contador: {contador}</p>
+      <p>Booleano: {bool.toString()}</p>
+
       <Boton alHacerClick={incrementar} texto="Incrementar" />
+      <Boton alHacerClick={cambiarBool} texto="Cambiar valor" />
     </>
   );
 }
