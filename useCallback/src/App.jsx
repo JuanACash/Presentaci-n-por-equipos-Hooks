6bb8f8ee581ccc44271684e7717e5e68405526c9
JuanAndresA016 <juancasthu@gmail.com>
2026-02-26 18:10:12 -0600

    Cambioz

--- useCallback/src/App.jsx
+++ useCallback/src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useCallback } from 'react';
 
-// Componente Hijo memorizado
 const Boton = React.memo(({ alHacerClick, texto }) => {
   console.log(`Botón ${texto} renderizado`);
   return <button onClick={alHacerClick}>{texto}</button>;
@@ -9,15 +8,13 @@ const Boton = React.memo(({ alHacerClick, texto }) => {
 export default function ContadorApp() {
   const [contador, setContador] = useState(0);
 
-  // Con useCallback, la función se mantiene estable
   const incrementar = useCallback(() => {
     setContador(c => c + 1);
-  }, []); // Dependencias vacías: no cambia nunca
+  }, []);
 
   return (
     <>
       <p>Contador: {contador}</p>
-      {/* Botón 2 se mantiene igual */}
       <Boton alHacerClick={incrementar} texto="Incrementar" />
     </>
   );
